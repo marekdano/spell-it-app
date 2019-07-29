795de473b53629de3c85aac49ac916860adac672
Marek Dano <mk.dano@gmail.com>
2019-07-29 14:11:07 +0100

    Add source object of words by levels.

---
+++ src/source.js
@@ -0,0 +1,29 @@
+export const wordsWithLevels = {
+  level1: [
+    'above', 'angel', 'answer', 'calf', 'does', 'earth',
+    'echo', 'extra', 'five', 'for', 'four', 'guess',
+    'half', 'health', 'iron', 'learn', 'nine', 'ocean',
+    'once', 'one', 'oven', 'pint', 'pull', 'range',
+    'says', 'six', 'ski', 'sure', 'swap', 'talk',
+    'ten', 'three', 'to', 'touch', 'two', 'view',
+    'warm', 'was', 'wash', 'word', 'zero' 
+  ],
+  level2: [
+    'another', 'beauty', 'beige', 'blood', 'bullet',
+    'carry', 'chalk', 'child', 'danger', 'early',
+    'eight', 'flood', 'floor', 'front', 'guide',
+    'haste', 'heaven', 'linger', 'mirror', 'other',
+    'priest', 'ready', 'rural', 'school', 'seven',
+    'squad', 'sugar', 'today', 'watch', 'water', 'yield'
+  ],
+  level3: [
+    'already', 'believe', 'built', 'bushel', 'comfort',
+    'coming', 'couple', 'cousin', 'enough', 'finger',
+    'guard', 'healthy', 'heavy', 'instead', 'laugh',
+    'measure', 'mother', 'niece', 'outdoor', 'period',
+    'plague', 'police', 'promise', 'quiet', 'ranger',
+    'relief', 'remove', 'search', 'shield', 'should',
+    'shovel', 'someone', 'source', 'statue', 'terror',
+    'trouble', 'welcome', 'wolves', 'woman', 'wonder'
+  ]
+};
